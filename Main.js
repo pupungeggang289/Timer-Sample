390c01baf40b9143fb52b0d0dcd9ecda24f7c8a2
pupungeggang289 <kyongkyong2892@gmail.com>
2022-12-27 01:30:16 +0900

    0:0

---
+++ Main.js
@@ -0,0 +1,17 @@
+window.onload = main;
+window.onerror = errorHandle;
+window.oncontextmenu = rightClick;
+
+var canvas;
+var context;
+var canvasRect;
+
+function main() {
+}
+
+function errorHandle() {
+}
+
+function rightClick() {
+    return false;
+}
